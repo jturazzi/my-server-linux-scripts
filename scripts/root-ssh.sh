6321dde914ece17a6dfcd8697117c61fff49a2d9
Jérémy Turazzi <git@jtrz.fr>
2023-11-02 19:38:55 +0100

    Add root-ssh

---
+++ scripts/root-ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# curl -L https://jturazzi.github.io/my-server-linux-scripts/scripts/root-sh.ssh | bash
+
+if ! command -v sudo &> /dev/null; then
+    echo "Erreur : sudo n'est pas installé sur ce système. Le script nécessite sudo pour s'exécuter."
+    exit 1
+fi
+
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo service ssh restart
