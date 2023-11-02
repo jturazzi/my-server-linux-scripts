2a30864a2c20e933acb74b7fa50943b0c8bb229d
Jérémy Turazzi <git@jtrz.fr>
2023-11-02 18:46:29 +0100

    Fix motd

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # curl -L https://jturazzi.github.io/my-server-linux-scripts/scripts/motd.sh | bash
 
-# Vérification que le script bash est lancer en root
-if [[ $(id -u) -ne 0 ]] ; then echo "Veuillez exécuter en mode root" ; exit 1 ; fi
+#!/bin/bash
 
-apt install neofetch -y
-mkdir /etc/neofetch
-curl -s https://jturazzi.github.io/my-server-linux-scripts/neofetch/neofetch.conf > /etc/neofetch/config.conf
-echo -n > /etc/motd
-rm /etc/update-motd.d/*
-tee -a /etc/update-motd.d/00-motd > /dev/null <<EOT 
+if ! command -v sudo &> /dev/null; then
+    echo "Erreur : sudo n'est pas installé sur ce système. Le script nécessite sudo pour s'exécuter."
+    exit 1
+fi
+
+sudo apt install neofetch -y
+sudo mkdir -p /etc/neofetch
+sudo sh -c 'curl -s https://jturazzi.github.io/my-server-linux-scripts/neofetch/config.conf > /etc/neofetch/config.conf'
+mkdir -p ~/.config/neofetch/ && [ -e ~/.config/neofetch/config.conf ] && rm ~/.config/neofetch/config.conf
+ln -s /etc/neofetch/config.conf ~/.config/neofetch/config.conf
+echo -n | sudo tee /etc/motd
+sudo rm /etc/update-motd.d/*
+sudo tee -a /etc/update-motd.d/00-motd > /dev/null <<EOT 
 #!/bin/sh
-neofetch --config /etc/neofetch/config.conf
+neofetch
 EOT
-chmod +x /etc/update-motd.d/00-motd
+sudo chmod +x /etc/update-motd.d/00-motd
