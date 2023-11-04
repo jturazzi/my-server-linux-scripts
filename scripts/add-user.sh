c49110675794d0a94cb58f3cdb8612831ceaf40c
Jérémy Turazzi <git@jtrz.fr>
2023-11-05 00:00:19 +0100

    Commit

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -9,6 +9,15 @@ fi
 echo -e -n "- Entrer le nom de l'utilisateur à créer : "
 read var1
 sudo adduser $var1
+
+read -p "Voulez-vous avoir l'accès sudo ? (oui/non) " response
+if [ "$response" = "oui" ]; then
+    sudo usermod -aG sudo $var1
+    echo "L'accès sudo vous a été accordé."
+else
+    echo "L'accès sudo n'a pas été accordé."
+fi
+
 sudo -u $var1 ssh-keygen -f /home/$var1/.ssh/id_rsa -P "" -q
 echo -e -n "- Clé SSH de l'utilisateur :"
 sudo cat /home/$var1/.ssh/id_rsa.pub
--- scripts/git.sh
+++ scripts/git.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # bash <(curl -sSL https://jturazzi.github.io/my-server-linux-scripts/scripts/git.sh)
 
+if ! command -v git &> /dev/null; then
+    echo "Erreur : git n'est pas installé sur ce système. Le script nécessite git pour s'exécuter."
+    exit 1
+fi
+
 echo -e -n "- Git - Entrer votre prénom et nom : "
 read var1
 echo -e -n "- Git - Entrer votre adresse email : "
